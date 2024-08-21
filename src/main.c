73fdca51c6d8e57425704f278331db2eff6cf43f
Tom Vidal <tomvidal421@gmail.com>
2024-08-21 12:34:38 +0200

    feat: add colour display option

--- src/main.c
+++ src/main.c
@@ -57,39 +57,113 @@ static void execute_interrupt(State8080 *state, int interrupt_number) {
     }
 }
 
-static void copy_screen_buffer_grayscale(uint8_t *screen_buffer,
-                                         uint8_t *memory, int scale) {
-    /* int index; */
-    /* int new_index; */
-    /* int new_row; */
-    /* int new_col; */
-    /* for (int i = 0; i < 256 * 224 / 8; i++) { */
-    /*     uint8_t byte = memory[i]; */
-    /*     for (int xs = 0; xs < scale; xs++) { */
-    /*         for (int ys = 0; ys < scale; ys++) { */
-    /*             for (int j = 0; j < 8; j++) { */
-    /*                 uint8_t bit = (byte >> j) % 2; */
-    /**/
-    /*                 index = 8 * i + j; */
-    /*                 new_row = 256 - index % 256; */
-    /*                 new_col = index / 256; */
-    /*                 new_index = new_row * 224 * scale * scale + new_col * scale; */
-    /*                 new_index += xs; */
-    /*                 new_index += scale * 224 * ys; */
-    /**/
-    /*                 screen_buffer[new_index] = 0xff * bit; */
-    /*             } */
-    /*         } */
-    /*     } */
-    /* } */
+static void copy_screen_buffer_grayscale(uint8_t *screen_buffer, uint8_t *memory) {
     int index;
     for (int i = 0; i < 256 * 224 / 8; i++) {
         uint8_t byte = memory[i];
         for (int j = 0; j < 8; j++) {
             uint8_t bit = (byte >> j) % 2;
 
-            index = 8 * i + j;
+            index = 3 * 8 * i + 3 * j;
             screen_buffer[index] = 0xff * bit;
+            screen_buffer[index + 1] = 0xff * bit;
+            screen_buffer[index + 2] = 0xff * bit;
+        }
+    }
+}
+
+static void copy_screen_buffer_colour(uint8_t *screen_buffer, uint8_t *memory) {
+    int index;
+    int row; // out of 256
+    int col; // out of 224
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    for (int i = 0; i < 256 * 224 / 8; i++) {
+        uint8_t byte = memory[i];
+        for (int j = 0; j < 8; j++) {
+            uint8_t bit = (byte >> j) % 2;
+
+            index = 3 * 8 * i + 3 * j;
+            col = 255 - (8 * i + j) % 256;
+            row = (8 * i + j) / 224;
+
+            if (32 <= col && col < 64) {
+                r = 0xff;
+                g = 0x00;
+                b = 0x00;
+            } else if (184 <= col && col < 240) {
+                r = 0x00;
+                g = 0xff;
+                b = 0x00;
+            } else if (col >= 240 && row >= 16 && row < 134) {
+                r = 0x00;
+                g = 0xff;
+                b = 0x00;
+            } else {
+                r = 0xff;
+                g = 0xff;
+                b = 0xff;
+            }
+            screen_buffer[index] = r * bit;
+            screen_buffer[index + 1] = g * bit;
+            screen_buffer[index + 2] = b * bit;
+        }
+    }
+}
+
+static void colorize_screen_buffer(uint8_t *screen_buffer) {
+    int index;
+    for (int row = 0; row < 224; row++) {
+        for (int col = 32; col < 64; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            screen_buffer[index + 1] = 0x00;
+            screen_buffer[index + 2] = 0x00;
+        }
+        for (int col = 184; col < 240; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            screen_buffer[index] = 0x00;
+            screen_buffer[index + 2] = 0x00;
+        }
+    }
+    for (int row = 16; row < 134; row++) {
+        for (int col = 240; col < 256; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            screen_buffer[index] = 0x00;
+            screen_buffer[index + 2] = 0x00;
+        }
+    }
+}
+
+static void decolorize_screen_buffer(uint8_t *screen_buffer) {
+    int index;
+    uint8_t colour;
+    for (int row = 0; row < 224; row++) {
+        for (int col = 32; col < 64; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            colour = screen_buffer[index];
+            screen_buffer[index + 1] = colour;
+            screen_buffer[index + 2] = colour;
+        }
+        for (int col = 184; col < 240; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            colour = screen_buffer[index + 1];
+            screen_buffer[index] = colour;
+            screen_buffer[index + 2] = colour;
+        }
+    }
+    for (int row = 16; row < 134; row++) {
+        for (int col = 240; col < 256; col++) {
+            index = 256 * row + 255 - col;
+            index *= 3;
+            colour = screen_buffer[index + 1];
+            screen_buffer[index] = colour;
+            screen_buffer[index + 2] = colour;
         }
     }
 }
@@ -162,17 +236,20 @@ int main() {
 
     long iteration_number = 0;
 
+    bool colour_display = true;
+
     // 2400-3FFF 7K video RAM
-    uint8_t *screen_buffer = malloc(scale * 8 * 256 * 224);
-    copy_screen_buffer_grayscale(screen_buffer, &state->memory[0x2400], scale);
+    uint8_t *screen_buffer = malloc(3 * 8 * 256 * 224);
+    copy_screen_buffer_grayscale(screen_buffer, &state->memory[0x2400]);
+    if (colour_display) {
+        colorize_screen_buffer(screen_buffer);
+    }
 
     // The raster resolution is 256x224 at 60Hz
     // The monitor is rotated in the cabinet 90 degrees counter-clockwise
     Image image = {
-        .format = (int)PIXELFORMAT_UNCOMPRESSED_GRAYSCALE,
+        .format = (int)PIXELFORMAT_UNCOMPRESSED_R8G8B8,
         .mipmaps = 1,
-        /* .width = scale * 224, */
-        /* .height = scale * 256, */
         .width = 256,
         .height = 224,
         .data = screen_buffer,
@@ -201,14 +278,14 @@ int main() {
     last_interrupt_time = time - interrupt_delay;
     time_since_last_draw = frame_dt;
 
-    bool update_screen = false;
-
     int tilt_bounce = 0; // in frames
 
     // Gui variables
     bool edit_scale = false;
-    bool paused = false;
+    bool paused = true;
     bool display_fps = false;
+    bool display_settings = true;
+    bool weird_tilt = true;
 
     int new_scale = scale;
 
@@ -236,12 +313,12 @@ int main() {
                 if (state->which_interrupt == 2) {
 
                     // 2400-3FFF 7K Video RAM
-                    copy_screen_buffer_grayscale(screen_buffer, &state8080_memory[0x2400],
-                                                 scale);
+                    copy_screen_buffer_grayscale(screen_buffer, &state8080_memory[0x2400]);
+                    if (colour_display) {
+                        colorize_screen_buffer(screen_buffer);
+                    }
 
                     UpdateTexture(texture, image.data);
-
-                    /* update_screen = true; */
                 }
 
                 // Handle input for the next frame
@@ -266,98 +343,144 @@ int main() {
 
         // When the game plays, the display is driven by update_screen
         // When the game is paused, we need to manually keep track of the elapsed time to update the gui
-        if (update_screen || GetTime() - time_since_last_draw > frame_dt) {
+        if (GetTime() - time_since_last_draw > frame_dt) {
             time_since_last_draw = GetTime();
-            /* update_screen = false; */
-            paused = false;
+
+            if (IsKeyPressed(KEY_TAB) && !display_settings) {
+                display_settings = true;
+                ShowCursor();
+                SetWindowSize(window_width, window_height);
+            } else if (IsKeyPressed(KEY_TAB) && display_settings) {
+                display_settings = false;
+                SetWindowSize(game_window_width, game_window_height);
+                HideCursor();
+            }
+
+            paused = display_settings;
 
             BeginDrawing();
             ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (tilt_bounce != 0) {
-                DrawTexturePro(texture, (Rectangle){0, 0, 256, 224}, (Rectangle){0, 0, scale * 256, scale * 224}, (Vector2){0, 0}, 10, WHITE);
+            Color tint;
+            if (display_settings) {
+                tint = GRAY;
             } else {
-                DrawTexturePro(texture, (Rectangle){0, 0, 256, 224}, (Rectangle){0, 0, scale * 256, scale * 224}, (Vector2){scale * 256, 0}, -90, WHITE);
+                tint = WHITE;
+            }
+
+            if (weird_tilt && tilt_bounce != 0 && !display_settings) {
+                DrawTexturePro(texture, (Rectangle){0, 0, 256, 224}, (Rectangle){0, 0, scale * 256, scale * 224}, (Vector2){0, 0}, 10, tint);
+            } else {
+                DrawTexturePro(texture, (Rectangle){0, 0, 256, 224}, (Rectangle){0, 0, scale * 256, scale * 224}, (Vector2){scale * 256, 0}, -90, tint);
             }
 
             if (display_fps) {
                 DrawFPS(10, 10);
             }
 
-            Rectangle tab_bounds = {
-                224 * base_scale + 6 * base_scale,
-                6 * base_scale,
-                100 * base_scale,
-                10 * base_scale,
-            };
-
-            CustomGuiTabBar(tab_bounds, &tab_text[0], nb_tabs, &active_tab);
-
-            if (active_tab == 0) {
-
-                Rectangle scale_box = {
-                    224 * base_scale + 6 * base_scale + (float)MeasureText("scale =", GuiGetStyle(DEFAULT, TEXT_SIZE)),
-                    6 * base_scale + 16 * base_scale,
-                    10 * base_scale,
-                    10 * base_scale,
-                };
-                if (GuiValueBox(scale_box, "scale =", &new_scale, 1, 4, edit_scale)) {
-                    edit_scale = !edit_scale;
-                } else {
-                    if (!edit_scale && new_scale != scale) {
-                        scale = new_scale;
-                        game_window_width = 224 * scale;
-                        game_window_height = 256 * scale;
-                    }
-                }
-                if (edit_scale) {
-                    paused = true;
-                }
-
-                Rectangle speed_slider_box = {
-                    224 * base_scale + 6 * base_scale + (float)MeasureText("speed ", GuiGetStyle(DEFAULT, TEXT_SIZE)) - (float)GuiGetStyle(SLIDER, TEXT_PADDING),
-                    6 * base_scale + 2 * 16 * base_scale,
-                    90 * base_scale,
-                    10 * base_scale,
-                };
-                GuiSlider(speed_slider_box, "speed ", TextFormat("%0.2f", speed), &speed, 1.0, 10.0);
-                if (guiSliderDragging) {
-                    paused = true;
-                }
-                // update variables using speed
-                interrupt_delay = 1.0 / (speed * 120.0);
-
-            } else if (active_tab == 1) {
-
-                int nb_controls = 7;
-                char *controls_text[7] = {
-                    "c = coin",
-                    "1 = start a game in one-player mode",
-                    "2 = start a game in two-player mode",
-                    "left = move the player left",
-                    "right = move the player right",
-                    "space = shoot",
-                    "t = tilt",
-                };
-                Rectangle control_box = {
+            if (display_settings) {
+                Rectangle tab_bounds = {
                     224 * base_scale + 6 * base_scale,
-                    0.5 * GuiGetStyle(DEFAULT, TEXT_SIZE) + 6 * base_scale + 16 * base_scale,
-                    220 * base_scale,
-                    0,
+                    6 * base_scale,
+                    100 * base_scale,
+                    10 * base_scale,
                 };
 
-                for (int i = 0; i < nb_controls; i++) {
-                    GuiLabel(control_box, controls_text[i]);
-                    control_box.y += 16 * base_scale;
+                CustomGuiTabBar(tab_bounds, &tab_text[0], nb_tabs, &active_tab);
+
+                if (active_tab == 0) {
+
+                    Rectangle scale_box = {
+                        224 * base_scale + 6 * base_scale + (float)MeasureText("scale =", GuiGetStyle(DEFAULT, TEXT_SIZE)),
+                        6 * base_scale + 16 * base_scale,
+                        10 * base_scale,
+                        10 * base_scale,
+                    };
+                    if (GuiValueBox(scale_box, "scale =", &new_scale, 1, 4, edit_scale)) {
+                        edit_scale = !edit_scale;
+                    } else {
+                        if (!edit_scale && new_scale != scale) {
+                            scale = new_scale;
+                            game_window_width = 224 * scale;
+                            game_window_height = 256 * scale;
+                        }
+                    }
+
+                    Rectangle speed_slider_box = {
+                        224 * base_scale + 6 * base_scale + (float)MeasureText("speed ", GuiGetStyle(DEFAULT, TEXT_SIZE)) - (float)GuiGetStyle(SLIDER, TEXT_PADDING),
+                        6 * base_scale + 2 * 16 * base_scale,
+                        90 * base_scale,
+                        10 * base_scale,
+                    };
+                    GuiSlider(speed_slider_box, "speed ", TextFormat("%0.2f", speed), &speed, 1.0, 10.0);
+                    /* if (guiSliderDragging) { */
+                    /*     paused = true; */
+                    /* } */
+
+                    // update variables using speed
+                    interrupt_delay = 1.0 / (speed * 120.0);
+
+                    Rectangle colour_checkbox_box = {
+                        224 * base_scale + 6 * base_scale,
+                        6 * base_scale + 3 * 16 * base_scale + 16,
+                        6 * base_scale,
+                        6 * base_scale,
+                    };
+
+                    bool colour_temp = colour_display;
+                    GuiCheckBox(colour_checkbox_box, "colour", &colour_display);
+                    if (colour_temp != colour_display) {
+                        if (colour_display) {
+                            colorize_screen_buffer(screen_buffer);
+                            UpdateTexture(texture, image.data);
+                        } else {
+                            decolorize_screen_buffer(screen_buffer);
+                            UpdateTexture(texture, image.data);
+                        }
+                    }
+
+                    Rectangle tilt_checkbox_box = {
+                        224 * base_scale + 6 * base_scale,
+                        6 * base_scale + 4 * 16 * base_scale + 16,
+                        6 * base_scale,
+                        6 * base_scale,
+                    };
+                    GuiCheckBox(tilt_checkbox_box, "tilt", &weird_tilt);
+
+                } else if (active_tab == 1) {
+
+                    int nb_controls = 9;
+                    char *controls_text[9] = {
+                        "tab = play/pause",
+                        "esc = quit",
+                        "c = coin",
+                        "1 = start a game in one-player mode",
+                        "2 = start a game in two-player mode",
+                        "left = move the player left",
+                        "right = move the player right",
+                        "space = shoot",
+                        "t = tilt",
+                    };
+                    Rectangle control_box = {
+                        224 * base_scale + 6 * base_scale,
+                        0.5 * GuiGetStyle(DEFAULT, TEXT_SIZE) + 6 * base_scale + 16 * base_scale,
+                        220 * base_scale,
+                        0,
+                    };
+
+                    for (int i = 0; i < nb_controls; i++) {
+                        GuiLabel(control_box, controls_text[i]);
+                        control_box.y += 16 * base_scale;
+                    }
+                } else if (active_tab == 2) {
+                    Rectangle fps_checkbox_box = {
+                        224 * base_scale + 6 * base_scale,
+                        6 * base_scale + 16 * base_scale,
+                        6 * base_scale,
+                        6 * base_scale,
+                    };
+                    GuiCheckBox(fps_checkbox_box, "display fps ", &display_fps);
                 }
-            } else if (active_tab == 2) {
-                Rectangle fps_checkbox_box = {
-                    224 * base_scale + 6 * base_scale, // + (float)MeasureText("display fps ", GuiGetStyle(DEFAULT, TEXT_SIZE)) - (float)GuiGetStyle(CHECKBOX, TEXT_PADDING),
-                    6 * base_scale + 16 * base_scale,
-                    6 * base_scale,
-                    6 * base_scale,
-                };
-                GuiCheckBox(fps_checkbox_box, "display fps ", &display_fps);
             }
 
             EndDrawing();
